0a86184e2302aa1239f8f7b44933e97d91e5bebb
Karen Gishyan <karengishyann@gmail.com>
2022-09-14 20:44:17 +0400

    prediction step added, training code updates

--- mimic3/networks.py
+++ mimic3/networks.py
@@ -30,18 +30,20 @@ def train():
     Subnetwork 1 (t1) and subnetwork2 (t2) each train two models, and the final trains trains
     1 model.
     """
-    def train_subnetwork(model, criterion, train_loader1,optimizer,train_loader2=None, epochs=10):
+
+    def train_subnetwork(model1, criterion, train_loader1,optimizer,model2=None,train_loader2=None, epochs=10):
         """
         Each subnetwork minimizes cobmined loss of two models.
         """
+
         total_loss = []
         epoch_loss=[]
         ACC = []
         if train_loader2:
             for epoch in range(epochs):
                 for i,((x1,y1), (x2,y2)) in enumerate(zip(train_loader1,train_loader2)):
-                    yhat1 = model(x1)
-                    yhat2 = model(x2)
+                    yhat1 = model1(x1)
+                    yhat2 = model2(x1)
                     loss = criterion(yhat1, y1)+criterion(yhat2, y2)
                     optimizer.zero_grad()
                     loss.backward()
@@ -53,7 +55,7 @@ def train():
             # if the final subnetwork
             for epoch in range(epochs):
                 for i,(x,y) in enumerate((train_loader1)):
-                    yhat = model(x)
+                    yhat = model1(x)
                     loss = criterion(yhat, y)
                     optimizer.zero_grad()
                     loss.backward()
@@ -70,35 +72,45 @@ def train():
         ax1.tick_params(axis='y', color=color)
 
         plt.show()
-        return total_loss
-
-    # number of feature columns is 6(input dim), number of output columns is 3(output dim).
-    model1 = Network(3,3)
-    model2 = Network(6,3)
-    final_model=Network(6,1)
-
-    optimizer1 = torch.optim.SGD(model1.parameters(), lr=0.001)
-    optimizer2 = torch.optim.SGD(model2.parameters(), lr=0.001)
-    optimizer3 = torch.optim.SGD(final_model.parameters(), lr=0.001)
-    criterion = nn.MSELoss()
-
-    dataset1_1 = Data(timestep='t1',return_feature=True)
-    dataset1_2 = Data(timestep='t1',return_feature=False)
-    dataset2_1 = Data(timestep='t2',return_feature=True)
-    dataset2_2 = Data(timestep='t2',return_feature=False)
-    dataset_final=Data(timestep='t3')
-
-    train_loader1_1 = DataLoader(dataset=dataset1_1, batch_size=10)
-    train_loader1_2 = DataLoader(dataset=dataset1_2, batch_size=10)
-    train_loader2_1 = DataLoader(dataset=dataset2_1, batch_size=10)
-    train_loader2_2 = DataLoader(dataset=dataset2_2, batch_size=10)
-    train_loader_final = DataLoader(dataset=dataset_final, batch_size=10)
 
     #train 1st subnetwork
-    train_subnetwork(model1, criterion, train_loader1_1, optimizer1,train_loader2=train_loader1_2, epochs=10)
+    train_subnetwork(model1,criterion, train_loader1_1, optimizer1,model2=model2,train_loader2=train_loader1_2, epochs=10)
     #train 2nd subnetwork
-    train_subnetwork(model2, criterion, train_loader2_1,optimizer2,train_loader2=train_loader2_2,epochs=10)
+    train_subnetwork(model3, criterion, train_loader2_1,optimizer2,model2=model4,train_loader2=train_loader2_2,epochs=10)
     #train 3rd subnetwork
     train_subnetwork(final_model, criterion, train_loader_final, optimizer3, epochs=10)
 
+    return model1, model2, model3, model4, final_model
+
+# number of feature columns is 6(input dim), number of output columns is 3(output dim).
+model1=model2= Network(3,3)
+model3=model4= Network(6,3)
+final_model=Network(6,1)
+
+optimizer1 = torch.optim.SGD(model1.parameters(), lr=0.001)
+optimizer2 = torch.optim.SGD(model2.parameters(), lr=0.001)
+optimizer3 = torch.optim.SGD(final_model.parameters(), lr=0.001)
+criterion = nn.MSELoss()
+
+dataset1_1 = Data(timestep='t1',return_feature=True)
+dataset1_2 = Data(timestep='t1',return_feature=False)
+dataset2_1 = Data(timestep='t2',return_feature=True)
+dataset2_2 = Data(timestep='t2',return_feature=False)
+dataset_final=Data(timestep='t3')
+
+train_loader1_1 = DataLoader(dataset=dataset1_1, batch_size=10)
+train_loader1_2 = DataLoader(dataset=dataset1_2, batch_size=10)
+train_loader2_1 = DataLoader(dataset=dataset2_1, batch_size=10)
+train_loader2_2 = DataLoader(dataset=dataset2_2, batch_size=10)
+train_loader_final = DataLoader(dataset=dataset_final, batch_size=10)
+
 train()
+# prediction
+with torch.no_grad():
+    features_t0,_=next(iter(train_loader1_1))
+    features_t1=model1(features_t0)
+    drugs_t1=model2(features_t0)
+    features_t2=model3(torch.concat([features_t1,drugs_t1],dim=1))
+    drugs_t2=model4(torch.concat([features_t1,drugs_t1],dim=1))
+    final_outcome=final_model(torch.concat([features_t2,drugs_t2],dim=1))
+
