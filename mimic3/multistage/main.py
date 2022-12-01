ec788c89384f919f20bafcb87e5d73ab67a5263f
Karen Gishyan <karengishyann@gmail.com>
2022-12-01 17:38:29 +0400

    multistage very base model

--- mimic3/multistage/main.py
+++ mimic3/multistage/main.py
@@ -1,6 +1,11 @@
 import os
 import yaml
 from utils import DataConversion
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from model import FeatureDataset, DrugDataset, Model, MultiStageModel
+import matplotlib.pyplot as plt
 
 dir_=os.path.dirname(__file__)
 os.chdir(dir_)
@@ -12,8 +17,76 @@ with open('../datasets/sqldata/stats.yaml') as stats, open('info.yaml') as info:
 diagnoses=stats['diagnosis_for_selection']
 timesteps=info['timesteps']
 for diagnosis in diagnoses:
+    features_datasets=[]
+    drug_datasets=[]
     for t in timesteps:
-        DataConversion(diagnosis=diagnosis,timestep=t).average_save_feature_time_series().\
-            convert_drugs_dummy_data_format()
+        # DataConversion(diagnosis=diagnosis,timestep=t).average_save_feature_time_series().\
+        #     convert_drugs_dummy_data_format()
 
+        features_datasets.append(FeatureDataset(diagnosis=diagnosis,timestep=t))
+        drug_datasets.append(DrugDataset(diagnosis=diagnosis,timestep=t))
 
+    features_t1,features_t2, features_t3=features_datasets
+    drug_t1,drug_t2, drug_t3=drug_datasets
+
+    loader_features_t1,loader_features_t2, loader_features_t3=map(lambda i:DataLoader(i,batch_size=1), features_datasets)
+    loader_drug_t1,loader_drug_t2, loader_drug_t3=map(lambda i:DataLoader(i,batch_size=1), drug_datasets)
+
+    feature_model=Model(912,10)
+    drug_model=Model(912,902)
+    outpt_model=Model(912,1)
+
+    multistage_model=MultiStageModel(feature_model,drug_model,outpt_model)
+    optimizer = torch.optim.Adam(multistage_model.parameters(), lr=0.0001)
+    criterion= nn.MSELoss()
+    epochs=50
+
+    combined_loader=zip(loader_features_t1,loader_drug_t1,loader_features_t2,\
+                     loader_drug_t2,loader_features_t3,loader_drug_t3)
+
+    l11_loss,l21_loss,lout_loss=[],[],[]
+    l12_loss,l22_loss=[],[]
+    combined_loss=[]
+
+    for epoch in range(epochs):
+        for i,((x1_t1,y11), (x2_t1,y21),(x1_t2,y12),(x2_t2,y22), (x1_t3,y),(x2_t3,y)) \
+                 in enumerate(combined_loader):
+            feature_Xt2,drug_Xt2,feature_Xt3,drug_Xt3,out=multistage_model(x1_t1,x2_t1)
+
+            l11=criterion(feature_Xt2,y11)
+            l12=criterion(drug_Xt2,y21)
+            l21=criterion(feature_Xt3,y12)
+
+            #drug_t2
+            l22=criterion(drug_Xt3,y22)
+
+            #output
+            lout=criterion(out,y)
+
+            loss=((l11+l12+l21+l22+lout)/1) # one is batch size
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        l11_loss.append(round(l11.item(),3))
+        l12_loss.append(round(l12.item(),3))
+        l21_loss.append(round(l21.item(),3))
+        l22_loss.append(round(l22.item(),3))
+        lout_loss.append(round(lout.item(),2))
+        combined_loss.append(loss.item())
+
+    _, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1, 5, sharey=False)
+    ax1.plot(l11_loss)
+    ax1.set_title('T1 Features')
+    ax2.plot(l12_loss)
+    ax2.set_title('T1 Drugs')
+    ax3.plot(l21_loss)
+    ax3.set_title('T2 Features')
+    ax4.plot(l22_loss)
+    ax4.set_title('T2 Drugs')
+    ax5.plot(lout_loss)
+    ax5.set_title('Output')
+    plt.ylabel('MSE Loss per Epoch')
+    plt.xlabel('Epochs')
+    plt.show()
---
+++ mimic3/multistage/model.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+from torch import sigmoid
+
+
+class BaseDataset(Dataset):
+    def __init__(self,diagnosis, timestep):
+        super().__init__()
+        self.diagnosis=diagnosis
+        self.timestep=timestep
+        self.validate_timestep()
+
+    def validate_timestep(self):
+        if self.timestep not in [1,2,3]:
+            raise ValueError(f"timestep {self.timestep} not supported.")
+
+    def train_test_split(self):
+        train_size = int(0.8 * len(self.X))
+        self.train_X, self.test_X = self.X[:train_size],self.X[train_size:]
+        self.train_Y,self.test_y=self.Y[:train_size],self.Y[train_size:]
+
+    def __getitem__(self, index) :
+        # test will not be iterated.
+        return self.train_X[index].float(),self.train_Y[index].float()
+
+    def __len__(self):
+        return len(self.train_X)
+
+
+class FeatureDataset(BaseDataset):
+    def __init__(self,diagnosis, timestep):
+        super().__init__(diagnosis, timestep)
+        self.X=torch.load(f"{diagnosis}/t{timestep}/features.pt")
+        if timestep in [1,2]:
+            self.Y=torch.load(f"{diagnosis}/t{timestep+1}/features.pt")
+        else:
+            self.Y=torch.load(f"../datasets/{diagnosis}/output.pt")
+        self.train_test_split()
+
+
+class DrugDataset(FeatureDataset):
+    def __init__(self,diagnosis, timestep):
+        super().__init__(diagnosis, timestep)
+        self.X=torch.load(f"{diagnosis}/t{timestep}/drugs.pt")
+        if timestep in [1,2]:
+            self.Y=torch.load(f"{diagnosis}/t{timestep+1}/drugs.pt")
+        else:
+            self.Y=torch.load(f"../datasets/{diagnosis}/output.pt")
+        self.train_test_split()
+
+
+class Model(nn.Module):
+    def __init__(self,input_size,output_size):
+        super().__init__()
+        self.linear=nn.Linear(input_size,output_size)
+
+    def forward(self,x):
+         out=sigmoid(self.linear(x))
+         return out
+
+class MultiStageModel(nn.Module):
+    def __init__(self,feature_model, drug_model,output_model):
+        super().__init__()
+        self.feature_model=feature_model
+        self.drug_model=drug_model
+        self.output_model=output_model
+
+    def forward(self,feature_Xt1,drug_Xt1):
+        #t2 pred
+        X=torch.cat((feature_Xt1,drug_Xt1),dim=1)
+        feature_Xt2=self.feature_model(X)
+        drug_Xt2=self.drug_model(X)
+
+        #t3 pred
+        X=torch.cat((feature_Xt2,drug_Xt2),dim=1)
+        feature_Xt3=self.feature_model(X)
+        drug_Xt3=self.drug_model(X)
+
+        #output pred
+        X=torch.cat((feature_Xt3,drug_Xt3),dim=1)
+        out=self.output_model(X)
+
+        return feature_Xt2,drug_Xt2,feature_Xt3,drug_Xt3,out
+
