2c0836b61ef0862bf5fb5bd9fb13bf7e867ea1f3
Karen Gishyan <karengishyann@gmail.com>
2022-12-04 12:10:22 +0400

    learning trends per diagnosis per epoch

--- mimic3/multistage/main.py
+++ mimic3/multistage/main.py
@@ -28,16 +28,18 @@ for diagnosis in diagnoses:
 
     features_t1,features_t2, features_t3=features_datasets
     drug_t1,drug_t2, drug_t3=drug_datasets
+    torch.seed()
+    batch_size=30
 
-    loader_features_t1,loader_features_t2, loader_features_t3=map(lambda i:DataLoader(i,batch_size=1), features_datasets)
-    loader_drug_t1,loader_drug_t2, loader_drug_t3=map(lambda i:DataLoader(i,batch_size=1), drug_datasets)
+    loader_features_t1,loader_features_t2, loader_features_t3=map(lambda i:DataLoader(i,batch_size=batch_size), features_datasets)
+    loader_drug_t1,loader_drug_t2, loader_drug_t3=map(lambda i:DataLoader(i,batch_size=batch_size), drug_datasets)
 
     feature_model=Model(912,10)
     drug_model=Model(912,902)
     outpt_model=Model(912,1)
 
     multistage_model=MultiStageModel(feature_model,drug_model,outpt_model)
-    optimizer = torch.optim.Adam(multistage_model.parameters(), lr=0.0001)
+    optimizer = torch.optim.SGD(multistage_model.parameters(), lr=0.01)
     criterion= nn.MSELoss()
     epochs=50
 
@@ -48,9 +50,11 @@ for diagnosis in diagnoses:
     l12_loss,l22_loss=[],[]
     combined_loss=[]
 
+
     for epoch in range(epochs):
         for i,((x1_t1,y11), (x2_t1,y21),(x1_t2,y12),(x2_t2,y22), (x1_t3,y),(x2_t3,y)) \
-                 in enumerate(combined_loader):
+                 in enumerate(zip(loader_features_t1,loader_drug_t1,loader_features_t2,\
+                     loader_drug_t2,loader_features_t3,loader_drug_t3)):
             feature_Xt2,drug_Xt2,feature_Xt3,drug_Xt3,out=multistage_model(x1_t1,x2_t1)
 
             l11=criterion(feature_Xt2,y11)
@@ -61,13 +65,15 @@ for diagnosis in diagnoses:
             l22=criterion(drug_Xt3,y22)
 
             #output
-            lout=criterion(out,y)
+            lout=criterion(out,y.view(-1,1))
 
-            loss=((l11+l12+l21+l22+lout)/1) # one is batch size
+            loss=(l11+l12+l21+l22+lout)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+        #TODO appending per batch does not work for the first diagnosis
 
         l11_loss.append(round(l11.item(),3))
         l12_loss.append(round(l12.item(),3))
@@ -76,7 +82,8 @@ for diagnosis in diagnoses:
         lout_loss.append(round(lout.item(),2))
         combined_loss.append(loss.item())
 
-    _, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1, 5, sharey=False)
+    fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1, 5, sharey=False)
+    plt.title(f"Loss Curves over 3 staged for {diagnosis} diagonis")
     ax1.plot(l11_loss)
     ax1.set_title('T1 Features')
     ax2.plot(l12_loss)
@@ -87,6 +94,12 @@ for diagnosis in diagnoses:
     ax4.set_title('T2 Drugs')
     ax5.plot(lout_loss)
     ax5.set_title('Output')
-    plt.ylabel('MSE Loss per Epoch')
-    plt.xlabel('Epochs')
+
+    fig.supxlabel('Epochs')
+    fig.supylabel('MSE Loss per Epoch')
+    fig.suptitle(f"{diagnosis}")
     plt.show()
+
+#TODO train-test
+#TODO experimentation with hidden layers
+#TODO better increase in learning
