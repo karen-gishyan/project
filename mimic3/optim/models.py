5ece22e17163c85fb72a83a9e71a764b011f5dd4
Karen Gishyan <karengishyann@gmail.com>
2023-12-08 11:39:31 +0400

    decreasing loss with oscilation, including normalization.

--- mimic3/optim/models.py
+++ mimic3/optim/models.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 class MultiClassLogisticRegression:
@@ -6,35 +8,37 @@ class MultiClassLogisticRegression:
         Linear Prediction -> Softmax Activation -> Cross Entropy Calculation -> Derivative calculation -> Update
         Sources:https://github.com/bamtak/machine-learning-implemetation-python/blob/master/Multi%20Class%20Logistic%20Regression.ipynb
     """
-    def __init__(self,n_iter = 1000, threshold=1e-3):
+    def __init__(self,n_iter = 500, threshold=0.001):
         self.n_iter = n_iter
         self.threshold = threshold
 
-    def fit(self,X, y, batch_size=10, lr=0.001, random_seed=4):
+    def fit(self,X, y, batch_size=303, lr=0.01, random_seed=4):
         """Model training."""
         np.random.seed(random_seed)
         self.classes = np.unique(y)
         self.class_labels = {c:i for i,c in enumerate(self.classes)}
         y = self.one_hot(y)
         X=X.to_numpy()
+        X=(X-np.min(X))/(np.max(X)-np.min(X))
+        self.X_mean=X.mean(axis=0).reshape(-1,1)
         self.loss = []
         self.bias = np.zeros((1, len(self.classes)))
         self.weights = np.zeros(shape=(len(self.classes),X.shape[1]))
-        i = 0
-        while (not self.n_iter or i < self.n_iter):
+        for i in range(self.n_iter):
             idx = np.random.choice(X.shape[0], batch_size)
             X_batch, y_batch = X[idx], y[idx]
             y_pred=self.predict(X_batch)
-            self.loss.append(self.cross_entropy(y_batch,y_pred))
+            loss=self.cross_entropy(y_batch,y_pred)
+            self.loss.append(loss)
+            print(loss)
             # update
             dweight,dbias=self.get_gradients(y_batch,y_pred,X_batch)
             self.weights-=lr*dweight
             self.bias-=lr*dbias
             if np.abs(dweight).max() < self.threshold:
                 break
-            if i % 100 == 0:
-                print(' Training Accuray at {} iterations is {}'.format(i, self.evaluate(X, y)))
-            i +=1
+            # if i % 100 == 0:
+            #     print(' Training Accuray at {} iterations is {}'.format(i, self.evaluate(X, y)))
 
     def one_hot(self, y):
         """Dummy conversion."""
@@ -52,6 +56,12 @@ class MultiClassLogisticRegression:
     def get_gradients(self,y,y_pred,X_batch):
         """ Calculates gradients of the loss function with respect to weights and bias."""
         dweight=-1* np.dot((y-y_pred).T,X_batch)/len(X_batch)
+        dbias=-1* np.sum((y-y_pred),axis=0)/len(X_batch)
+        return dweight,dbias
+
+    def get_gradients_with_penalty(self,y,y_pred,X_batch):
+        penalty=np.dot(X_batch,self.X_mean)/(np.linalg.norm(X_batch)*np.linalg.norm(self.X_mean))
+        dweight=-1* np.dot((y-y_pred).T,X_batch)/len(X_batch)-np.mean(penalty)
         dbias=-1* np.sum((y-y_pred),axis=0)/len(X_batch)
         return dweight,dbias
 
@@ -77,3 +87,7 @@ class MultiClassLogisticRegression:
         X_test=X_test.to_numpy()
         y_test=y_test.to_numpy().flatten()
         return np.mean(self.predict_classes(X_test) ==y_test)
+
+    def plot(self):
+        plt.plot(self.loss)
+        plt.show()
