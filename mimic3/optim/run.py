b479c4c7a8d92e97e6af0bf2a16bc83b7c3f77b6
Karen Gishyan <karengishyann@gmail.com>
2023-12-12 23:42:59 +0400

    add more datasets, log results and comparisons

--- mimic3/optim/run.py
+++ mimic3/optim/run.py
@@ -1,4 +1,4 @@
-from ucimlrepo import fetch_ucirepo
+from ucimlrepo import fetch_ucirepo,list_available_datasets
 from sklearn.model_selection import train_test_split
 from utils import remove_nan,evaluate, compare
 import warnings
@@ -13,6 +13,7 @@ y = heart_disease.data.targets
 X,y=remove_nan(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/heart_disease.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/heart_disease_no_scale.log',scale=False)
 compare('project/mimic3/optim/results/heart_disease.log','project/mimic3/optim/results/heart_disease_no_scale.log')
 
 hepatitis = fetch_ucirepo(id=46)
@@ -22,6 +23,7 @@ y = hepatitis.data.targets
 X,y=remove_nan(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/hepatitis.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/hepatitis_no_scale.log',scale=False)
 compare('project/mimic3/optim/results/hepatitis.log','project/mimic3/optim/results/hepatitis_no_scale.log')
 
 
@@ -32,4 +34,62 @@ y = lung_cancer.data.targets
 X,y=remove_nan(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/lung_cancer.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/lung_cancer_no_scale.log',scale=False)
 compare('project/mimic3/optim/results/lung_cancer.log','project/mimic3/optim/results/lung_cancer_no_scale.log')
+
+
+diabetic_retinopathy_debrecen = fetch_ucirepo(id=329)
+print("diabetic_retinopathy_debrecen dataset shape {}".format(diabetic_retinopathy_debrecen.data.features.shape))
+X = diabetic_retinopathy_debrecen.data.features
+y = diabetic_retinopathy_debrecen.data.targets
+X,y=remove_nan(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/diabetic.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/diabetic_no_scale.log',scale=False)
+compare('project/mimic3/optim/results/diabetic.log','project/mimic3/optim/results/diabetic_no_scale.log')
+
+
+bone_marrow_transplant_children = fetch_ucirepo(id=565)
+print("bone_marrow_transplant_children dataset shape {}".format(bone_marrow_transplant_children.data.features.shape))
+X = bone_marrow_transplant_children.data.features
+y = bone_marrow_transplant_children.data.targets
+X=X._get_numeric_data()
+X,y=remove_nan(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/bone_marrow.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/bone_marrow_no_scale.log',scale=False)
+compare('project/mimic3/optim/results/bone_marrow.log','project/mimic3/optim/results/bone_marrow_no_scale.log')
+
+
+glioma_grading_clinical_and_mutation_features = fetch_ucirepo(id=759)
+print("glioma_grading_clinical_and_mutation_features dataset shape {}".format(glioma_grading_clinical_and_mutation_features.data.features.shape))
+X = glioma_grading_clinical_and_mutation_features.data.features
+y = glioma_grading_clinical_and_mutation_features.data.targets
+X=X._get_numeric_data()
+X,y=remove_nan(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/glioma.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/glioma_no_scale.log',scale=False)
+compare('project/mimic3/optim/results/glioma.log','project/mimic3/optim/results/glioma_no_scale.log')
+
+heart_failure_clinical_records = fetch_ucirepo(id=519)
+print("heart_failure_clinical_records dataset shape {}".format(heart_failure_clinical_records.data.features.shape))
+X = heart_failure_clinical_records.data.features
+y = heart_failure_clinical_records.data.targets
+X,y=remove_nan(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/heart_failure.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/heart_failure_no_scale.log',scale=False)
+compare('project/mimic3/optim/results/heart_failure.log','project/mimic3/optim/results/heart_failure_no_scale.log')
+
+
+ilpd_indian_liver_patient_dataset = fetch_ucirepo(id=225)
+print("ilpd_indian_liver_patient_dataset dataset shape {}".format(ilpd_indian_liver_patient_dataset.data.features.shape))
+X = ilpd_indian_liver_patient_dataset.data.features
+y = ilpd_indian_liver_patient_dataset.data.targets
+X=X._get_numeric_data()
+X,y=remove_nan(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/ilpd.log')
+evaluate(X_train, X_test, y_train, y_test,save_path='project/mimic3/optim/results/ilpd_no_scale.log',scale=False)
+compare('project/mimic3/optim/results/ilpd.log','project/mimic3/optim/results/ilpd_no_scale.log')
--- mimic3/optim/utils.py
+++ mimic3/optim/utils.py
@@ -3,8 +3,11 @@ from sklearn.metrics import precision_recall_fscore_support
 from models import MultiClassLogisticRegression
 import numpy as np
 import json
-import os
+import logging
 
+logger=logging.getLogger('comparison_logger')
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.FileHandler(filename="project/mimic3/optim/results/comparisons.log",mode='w'))
 
 
 def remove_nan(X,y):
@@ -15,7 +18,7 @@ def remove_nan(X,y):
     return X,y
 
 
-def evaluate(X_train, X_test, y_train, y_test,save_path=None):
+def evaluate(X_train, X_test, y_train, y_test,save_path=None,scale=True):
     """For no scaling, use lr parameter instead of scaling_factor."""
     if not save_path:raise ValueError("Log path is not provided")
     step_size=int(len(X_train)/10)
@@ -26,7 +29,10 @@ def evaluate(X_train, X_test, y_train, y_test,save_path=None):
     save_list=[]
     for batch_size in batches:
         for scaling_factor in scaling_factors:
-            lr.fit(X_train,y_train,batch_size=batch_size,scaling_factor=scaling_factor)
+            if scale:
+                lr.fit(X_train,y_train,batch_size=batch_size,scaling_factor=scaling_factor)
+            else:
+                lr.fit(X_train,y_train,batch_size=batch_size,lr=scaling_factor)
             accuracy=round(lr.score(X_test,y_test),2)
             p,r,f1,_=precision_recall_fscore_support(y_test,lr.predict_classes(X_test),average='macro')
             custom_scd_dict,scd_dict={},{}
@@ -59,9 +65,9 @@ def evaluate(X_train, X_test, y_train, y_test,save_path=None):
             save_list.append(custom_scd_dict)
             save_list.append(scd_dict)
 
-    if not os.path.exists(save_path):
-        with open(save_path,'w') as file:
-            json.dump(save_list,file,indent=4)
+
+    with open(save_path,'w') as file:
+        json.dump(save_list,file,indent=4)
 
 
 def compare(path1,path2):
@@ -70,43 +76,36 @@ def compare(path1,path2):
         save_list=json.load(file)
         save_list_no_scale=json.load(file_no_scale)
 
+    logger.info(path1)
     #custom scd with scaling
     filtered_save_list=list(filter(lambda d: d['name'] =='custom_scd', save_list))
     max_f1=max(filtered_save_list,key=lambda d:d['f1'])['f1']
-    print("Max f1 scaled:{}".format(max_f1))
+    logger.info("Max f1 scaled:{}".format(max_f1))
     max_accuracy=max(save_list,key=lambda d:d['accuracy'])['accuracy']
-    print("Max accuracy scaled:{}".format(max_accuracy))
+    logger.info("Max accuracy scaled:{}".format(max_accuracy))
     average_f1=sum([d['f1'] for d in filtered_save_list])/len(filtered_save_list)
-    print("Mean f1 scaled:{}".format(average_f1))
+    logger.info("Mean f1 scaled:{}".format(average_f1))
     average_accuracy=sum([d['accuracy'] for d in filtered_save_list])/len(filtered_save_list)
-    print("Mean accuracy scaled:{}\n".format(average_accuracy))
+    logger.info("Mean accuracy scaled:{}\n".format(average_accuracy))
 
     #custom scd without scaling
     filtered_save_list_no_scale=list(filter(lambda d: d['name'] =='custom_scd', save_list_no_scale))
     max_f1=max(filtered_save_list_no_scale,key=lambda d:d['f1'])['f1']
-    print("Max f1 not scaled:{}".format(max_f1))
+    logger.info("Max f1 not scaled:{}".format(max_f1))
     max_accuracy=max(filtered_save_list_no_scale,key=lambda d:d['accuracy'])['accuracy']
-    print("Max accuracy not scaled:{}".format(max_accuracy))
+    logger.info("Max accuracy not scaled:{}".format(max_accuracy))
     average_f1=sum([d['f1'] for d in filtered_save_list_no_scale])/len(filtered_save_list_no_scale)
-    print("Mean f1 not scaled:{}".format(average_f1))
+    logger.info("Mean f1 not scaled:{}".format(average_f1))
     average_accuracy=sum([d['accuracy'] for d in filtered_save_list_no_scale])/len(filtered_save_list_no_scale)
-    print("Mean accuracy not scaled:{}\n".format(average_accuracy))
+    logger.info("Mean accuracy not scaled:{}\n".format(average_accuracy))
 
     #scd
     filtered_saved_list_default=list(filter(lambda d: d['name'] =='scd', save_list))
     max_f1=max(filtered_saved_list_default,key=lambda d:d['f1'])['f1']
-    print("Max f1 sklearn:{}".format(max_f1))
+    logger.info("Max f1 sklearn:{}".format(max_f1))
     max_accuracy=max(filtered_saved_list_default,key=lambda d:d['accuracy'])['accuracy']
-    print("Max accuracy sklearn:{}".format(max_accuracy))
+    logger.info("Max accuracy sklearn:{}".format(max_accuracy))
     average_f1=sum([d['f1'] for d in filtered_saved_list_default])/len(filtered_saved_list_default)
-    print("Mean f1 sklearn:{}".format(average_f1))
+    logger.info("Mean f1 sklearn:{}".format(average_f1))
     average_accuracy=sum([d['accuracy'] for d in filtered_saved_list_default])/len(filtered_saved_list_default)
-    print("Mean accuracy sklearn:{}\n".format(average_accuracy))
-
-
-    # count=0
-    # for d1,d2 in zip(save_list,save_list_no_scale):
-    #     if d1['name']=="custom_scd":
-    #         if d1['f1']>d2['f1']:
-    #             count+=1
-    # print("{}% of cases scaling is better".format(count/(len(save_list)/2)*100))
+    logger.info("Mean accuracy sklearn:{}\n".format(average_accuracy))
