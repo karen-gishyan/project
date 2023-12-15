ef158f21afdc42c5a73a9f01eed62e016a86718f
Karen Gishyan <karengishyann@gmail.com>
2023-12-15 10:42:32 +0400

    perform stochastic scd (batch size of 1) for better comparison, remove logs, currently learning is not good

--- mimic3/optim/utils.py
+++ mimic3/optim/utils.py
@@ -18,7 +18,7 @@ def remove_nan(X,y):
     return X,y
 
 
-def evaluate(X_train, X_test, y_train, y_test,save_path=None,scale=True,epochs=1):
+def evaluate(X_train, X_test, y_train, y_test,save_path=None,scale=True,epochs=10):
     """For no scaling, use lr parameter instead of scaling_factor.
     Feeds different chunks of data to the fit function, then performs stochastic gradient descent.
     Same approach as partial_fit() from sklearn.
@@ -31,34 +31,32 @@ def evaluate(X_train, X_test, y_train, y_test,save_path=None,scale=True,epochs=1
     # scaling factors the same as learning rates
     scaling_factors=[0.0001,0.001,0.01,0.05,0.1]
     save_list=[]
-    # batch does not do minibatching, rather a stochastic gd on a given batch
-    for batch_size in batches:
-        for scaling_factor in scaling_factors:
-            lr=MultiClassLogisticRegression()
-            for _ in range(epochs):
-                if scale:
-                    lr.fit(X_train,y_train,scaling_factor=scaling_factor,batch_size=batch_size)
-                else:
-                    lr.fit(X_train,y_train,lr=scaling_factor,batch_size=batch_size)
-            accuracy=round(lr.score(X_test,y_test),2)
-            p,r,f1,_=precision_recall_fscore_support(y_test,lr.predict_classes(X_test),average='macro')
-            custom_scd_dict={}
-            custom_scd_dict.update({
-                "name":"custom_scd",
-                "batch_size":batch_size,
-                "scaling_factor":scaling_factor,
-                "accuracy":accuracy,
-                "precision":p,
-                "recall":r,
-                "f1":f1
-            })
-            save_list.append(custom_scd_dict)
+    for scaling_factor in scaling_factors:
+        lr=MultiClassLogisticRegression()
+        for _ in range(epochs):
+            if scale:
+                lr.fit(X_train,y_train,scaling_factor=scaling_factor,batch_size=1)
+            else:
+                lr.fit(X_train,y_train,lr=scaling_factor,batch_size=1)
+        accuracy=round(lr.score(X_test,y_test),2)
+        p,r,f1,_=precision_recall_fscore_support(y_test,lr.predict_classes(X_test),average='macro')
+        custom_scd_dict={}
+        custom_scd_dict.update({
+            "name":"custom_scd",
+            "batch_size":1,
+            "scaling_factor":scaling_factor,
+            "accuracy":accuracy,
+            "precision":p,
+            "recall":r,
+            "f1":f1
+        })
+        save_list.append(custom_scd_dict)
 
     with open(save_path,'w') as file:
         json.dump(save_list,file,indent=4)
 
 
-def evaluate_sklearn(X_train, X_test, y_train, y_test,save_path=None,epochs=1):
+def evaluate_sklearn(X_train, X_test, y_train, y_test,save_path=None,epochs=10):
     if not save_path:raise ValueError("save path is not provided")
     step_size=int(len(X_train)/10)
     batches=list(range(step_size,len(X_train),step_size))
@@ -68,24 +66,23 @@ def evaluate_sklearn(X_train, X_test, y_train, y_test,save_path=None,epochs=1):
     # scaling factors the same as learning rates
     scaling_factors=[0.0001,0.001,0.01,0.05,0.1]
     save_list=[]
-    for batch_size in batches:
-        for scaling_factor in scaling_factors:
-            clf=SGDClassifier(loss='log_loss',shuffle=False,alpha=0,penalty=None,l1_ratio=0,tol=None,
-                              learning_rate='constant',eta0=scaling_factor,power_t=0,max_iter=epochs)
-            clf.fit(X_train,y_train)
-            accuracy=round(clf.score(X_test,y_test),2)
-            p,r,f1,_=precision_recall_fscore_support(y_test,clf.predict(X_test),average='macro')
-            scd_dict={}
-            scd_dict.update({
-                "name":"scd",
-                "batch_size":batch_size,
-                "scaling_factor":scaling_factor,
-                "accuracy":accuracy,
-                "precision":p,
-                "recall":r,
-                "f1":f1
-            })
-            save_list.append(scd_dict)
+    for scaling_factor in scaling_factors:
+        clf=SGDClassifier(loss='log_loss',shuffle=False,alpha=0,penalty=None,l1_ratio=0,tol=None,
+                            learning_rate='constant',eta0=scaling_factor,power_t=0,max_iter=epochs)
+        clf.fit(X_train,y_train)
+        accuracy=round(clf.score(X_test,y_test),2)
+        p,r,f1,_=precision_recall_fscore_support(y_test,clf.predict(X_test),average='macro')
+        scd_dict={}
+        scd_dict.update({
+            "name":"scd",
+            "batch_size":1,
+            "scaling_factor":scaling_factor,
+            "accuracy":accuracy,
+            "precision":p,
+            "recall":r,
+            "f1":f1
+        })
+        save_list.append(scd_dict)
 
     with open(save_path,'w') as file:
         json.dump(save_list,file,indent=4)
