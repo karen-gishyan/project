0257c356234bb656e38d7b29241d213a039f13c3
Karen Gishyan <karengishyann@gmail.com>
2024-10-21 13:01:05 +0400

    multilayer value_iteration experiment and comparison

--- mimic3/algos/model.py
+++ mimic3/algos/model.py
@@ -8,6 +8,7 @@ from numpy.linalg import norm
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_fscore_support
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
 
@@ -175,45 +176,69 @@ class MDP:
         return action_values
 
 
-    def value_iteration(self,theta=0.001):
+    def value_iteration(self,theta=0.001, **kwargs):
         """Perform value iteration, returning optimal policy."""
         #TODO converting to list may not be needed here in a few other similar places
         #NOTE number of actions need to be more than 1 for the algorithm to work in reasonable computation time
+        prev_layer_mdp = kwargs.get("prev_layer_mdp")
         states=list(self.graph.nodes)
+        convergence_count = 0
         while True:
+            convergence_count+=1
             delta=0
             for state in states:
                 if self.graph.nodes[state].get('goal'):
                     continue
                 current_value=self.graph.nodes[state]['value']
                 action_values=self.one_step_look_ahead(state=state)
-                max_action_value=max(action_values)
+                # take into account the value from the previous layer
+                if not prev_layer_mdp:
+                    max_action_value=max(action_values)
+                else:
+                    try:
+                        max_action_value=(max(action_values)+prev_layer_mdp.graph.nodes[state]['value'])/2
+                    except Exception as e:
+                        print(e)
                 self.graph.nodes[state]['value']=max_action_value
                 delta=max(delta,abs(current_value-max_action_value))
             if delta<=theta:
                 break
 
-        policy=[]
+        algorithm_response={
+            "time_period":self.time_period,
+            "n_actions_per_state":self.n_actions_per_state,
+            "steps_to_converge":convergence_count,
+            # "policy":[],
+            # "state_values":{},
+            "total_value_of_state_space":0,
+        }
         self.policy_graph=nx.DiGraph()
+        policy=[]
+        state_values={}
         for state in states:
-            logger.info(f"State:{state},Value:{self.graph.nodes[state]['value']}")
+            # logger.info(f"State:{state},Value:{self.graph.nodes[state]['value']}")
+            state_values.update({state:self.graph.nodes[state]['value']})
             action_values=self.one_step_look_ahead(state=state)
             best_action_id=np.argmax(action_values)
             best_action_state=list(self.graph.out_edges(state))[best_action_id][1]
-            policy.append(f"state: {state}->{best_action_state}.")
+            policy.append((state,best_action_state))
             self.policy_graph.add_edge(f"{state}_t{self.time_period}",f"{best_action_state}_t{self.time_period}")
-        logger.info(f"Policy\n {policy}")
-        return self.policy_graph
+        # logger.info(f"Policy\n {policy}")
+        total_value_of_state_space = sum(state_values.values())
+        algorithm_response.update({
+
+                                   "total_value_of_state_space":total_value_of_state_space})
+        return self.policy_graph, algorithm_response
 
 
 #When the model is learning, policy_outcome is mostly 1 (it learns to lead to the target class). It is a
 # a good RL model, but a bad classification model, as there are no zero outcomes.
 class StageMDP:
-    def __init__(self):
+    def __init__(self, n_actions_per_state=3):
         # more actions means more the chance of positive policy_outcome
-        self.mdp_t1=MDP(diagnosis="SEPSIS",n_actions_per_state=3).make_models().create_states_base(time_period=1)
-        self.mdp_t2=MDP(diagnosis="SEPSIS",n_actions_per_state=3).make_models().create_states_base(time_period=2)
-        self.mdp_t3=MDP(diagnosis="SEPSIS",n_actions_per_state=3).make_models().create_states_base(time_period=3)
+        self.mdp_t1=MDP(diagnosis="SEPSIS",n_actions_per_state=n_actions_per_state).make_models().create_states_base(time_period=1)
+        self.mdp_t2=MDP(diagnosis="SEPSIS",n_actions_per_state=n_actions_per_state).make_models().create_states_base(time_period=2)
+        self.mdp_t3=MDP(diagnosis="SEPSIS",n_actions_per_state=n_actions_per_state).make_models().create_states_base(time_period=3)
         self.mdp_list=[self.mdp_t1,self.mdp_t2,self.mdp_t3]
 
     def connect_graphs(self):
@@ -330,20 +355,100 @@ class StageMDP:
         with open("json_files/discrete_policies.json",'w') as file:
             json.dump(mappings,file,indent=4)
 
-    def __call__(self):
+    def create_state_spaces(self):
         self.connect_graphs()
-        obj1=self.mdp_t1.create_actions_and_transition_probabilities()
-        obj2=self.mdp_t2.create_actions_and_transition_probabilities()
-        obj3=self.mdp_t3.create_actions_and_transition_probabilities()
-        self.create_multilayer_graph()
+        self.obj1=self.mdp_t1.create_actions_and_transition_probabilities()
+        self.obj2=self.mdp_t2.create_actions_and_transition_probabilities()
+        self.obj3=self.mdp_t3.create_actions_and_transition_probabilities()
+        # self.create_multilayer_graph()
+        return self
 
-        t1_policy_graph=obj1.value_iteration()
-        t2_policy_graph=obj2.value_iteration()
-        t3_policy_graph=obj3.value_iteration()
-        self.combined_policy_graph=nx.compose_all([t1_policy_graph,t2_policy_graph,t3_policy_graph])
-        self.evaluate()
+    def evaluate_value_iteration(self):
+        t1_policy_graph, t1_algorithm_response=self.obj1.value_iteration()
+        t2_policy_graph, t2_algorithm_response=self.obj2.value_iteration()
+        t3_policy_graph, t3_algorithm_response=self.obj3.value_iteration()
+        # self.combined_policy_graph=nx.compose_all([t1_policy_graph,t2_policy_graph,t3_policy_graph])
+        # self.evaluate()
+        return [t1_algorithm_response, t2_algorithm_response, t3_algorithm_response]
+
+    def evaluate_multilayer_value_iteration(self):
+        t1_policy_graph, t1_algorithm_response=self.obj1.value_iteration()
+        t2_policy_graph, t2_algorithm_response=self.obj2.value_iteration(prev_layer_mdp=self.obj1)
+        t3_policy_graph, t3_algorithm_response=self.obj3.value_iteration(prev_layer_mdp=self.obj2)
+        # self.combined_policy_graph=nx.compose_all([t1_policy_graph,t2_policy_graph,t3_policy_graph])
+        # self.evaluate()
+        return [t1_algorithm_response, t2_algorithm_response, t3_algorithm_response]
+
+
+    def __call__(self):
+        value_iteration_results=[]
+        multilayer_value_iteration_results=[]
+
+        for count in range(1,23):
+            stage_mdp=StageMDP(n_actions_per_state=count)
+            algorithm_responses=stage_mdp.create_state_spaces().evaluate_value_iteration()
+            for response in algorithm_responses:
+                value_iteration_results.append((response))
+            algorithm_responses=stage_mdp.create_state_spaces().evaluate_multilayer_value_iteration()
+            for response in algorithm_responses:
+                multilayer_value_iteration_results.append(response)
+
+        with open("json_files/value_iterations_results.json",'w') as file:
+            json.dump(value_iteration_results,file,indent=4)
+
+        with open("json_files/multilayer_value_iterations_results.json",'w') as file:
+            json.dump(multilayer_value_iteration_results,file,indent=4)
+
+        x=[]
+        t1_ys,t2_ys,t3_ys=[], [],[]
+        for res in value_iteration_results:
+            if res["time_period"]==1:
+                x.append(res["n_actions_per_state"])
+                t1_ys.append(res["steps_to_converge"])
+            elif res["time_period"]==2:
+                t2_ys.append(res["steps_to_converge"])
+            elif res["time_period"]==3:
+                t3_ys.append(res["steps_to_converge"])
+
+        t1_ys_ml,t2_ys_ml,t3_ys_ml=[], [],[]
+
+        for res in multilayer_value_iteration_results:
+            if res["time_period"]==1:
+                t1_ys_ml.append(res["steps_to_converge"])
+            elif res["time_period"]==2:
+                t2_ys_ml.append(res["steps_to_converge"])
+            elif res["time_period"]==3:
+                t3_ys_ml.append(res["steps_to_converge"])
+
+        fig, axs = plt.subplots(3, 1, figsize=(8, 10))
+
+        axs[0].plot(x, t1_ys,label="standard")
+        axs[0].plot(x, t1_ys_ml,label="multilayer")
+        axs[0].set_title('t1')
+        axs[0].set_ylabel('Steps to converge')
+        axs[0].legend()
+
+        axs[1].plot(x, t2_ys, label="standard")
+        axs[1].plot(x, t2_ys_ml, label="multilayer")
+        axs[1].set_title('t2')
+        axs[1].set_ylabel('Steps to converge')
+        axs[1].legend()
+
+        axs[2].plot(x, t3_ys, label="standard")
+        axs[2].plot(x, t3_ys_ml, label="multilayer")
+        axs[2].set_title('t3')
+        axs[2].set_ylabel('Steps to converge')
+        axs[2].legend()
+
+        plt.tight_layout()  # Adjust layout to prevent overlap
+        plt.show()
+
+
+
+
+
+
 
 
 if __name__=="__main__":
-    #TODO argparser arguments for experimenting with results with different number of actions as inputs
     StageMDP()()
